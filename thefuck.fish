0976ff177b6623db6b3ebbcd81b2516fa5452e6e
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-10-11 20:28:25 -0300

    refact(function): improve line comment

--- thefuck.fish
+++ thefuck.fish
@@ -1,6 +1,6 @@
 function init --on-event init_thefuck
   if available thefuck
-    eval (thefuck --alias | tr '\n' ';')  # function stays always up-to-date
+    eval (thefuck --alias | tr '\n' ';')  # function stays up-to-date, always
   else
     echo "Please install thefuck first. Check https://github.com/nvbn/thefuck"
   end
