a8af75a3cafed2f4cfda9bf63ec387e22ef284d5
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-02-12 22:02:38 -0200

    refact(init): make it faster by means of universal vars

--- init.fish
+++ init.fish
@@ -1,6 +1,7 @@
 function init --on-event init_thefuck
   if type -q thefuck
-    eval (thefuck --alias | tr '\n' ';')  # function stays up-to-date, always
+    set -q __tf_func; or __tf_updt
+    eval $__tf_func
   else
     echo "Please install thefuck first. Check https://github.com/nvbn/thefuck"
   end
