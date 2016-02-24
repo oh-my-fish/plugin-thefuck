7759f5fbb95d8f31306fe04de2960e0f2bc53fe6
Justin Hileman <justin@justinhileman.info>
2016-02-24 00:29:07 -0800

    Replace `available` usage with `type -q`

--- init.fish
+++ init.fish
@@ -1,5 +1,5 @@
 function init --on-event init_thefuck
-  if available thefuck
+  if type -q thefuck
     eval (thefuck --alias | tr '\n' ';')  # function stays up-to-date, always
   else
     echo "Please install thefuck first. Check https://github.com/nvbn/thefuck"
