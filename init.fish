86f7e1b720f8395964f348f601b4b8fd9a1cf671
Justin Hileman <justin@justinhileman.info>
2016-03-06 13:05:31 -0800

    Initialize immediately, rather than waiting for an event.

--- init.fish
+++ init.fish
@@ -1,8 +1,6 @@
-function init --on-event init_thefuck
-  if type -q thefuck
-    set -q __tf_func; or __tf_updt
-    eval $__tf_func
-  else
-    echo "Please install thefuck first. Check https://github.com/nvbn/thefuck"
-  end
+if type -q thefuck
+  set -q __tf_func; or __tf_updt
+  eval $__tf_func
+else
+  echo "Please install thefuck first. Check https://github.com/nvbn/thefuck"
 end
